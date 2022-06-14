4b2e1bad442397ecfb30d61b7e5ef5c78eaea48a
kingston-tech <kingstonantony99@gmail.com>
2022-06-14 16:10:40 +0530

    conflicts resolved

--- src/org/space/Java.java
+++ src/org/space/Java.java
@@ -4,7 +4,7 @@ public class Java {
 	public static void main(String[] args) {
 		System.out.println("king");
 		System.out.println("ok");
-		System.out.println("watra this is ");
+		System.out.println("watra this is");
 		System.out.println("project");
 		System.out.println("class");
 		System.out.println("ok");
