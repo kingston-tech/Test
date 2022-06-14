2e44ce6976f8cae98a57a6dbb76b388e4c927a25
kingston-tech <kingstonantony99@gmail.com>
2022-06-14 16:28:56 +0530

    rewrite the code

--- src/org/space/Java.java
+++ src/org/space/Java.java
@@ -1,9 +1,20 @@
 package org.space;
 
+
 public class Java {
 	public static void main(String[] args) {
 		System.out.println("king");
 		System.out.println("ok");
+		
+		Java j =new Java();
+		Class<? extends Java> class1 = j.getClass();
+		System.err.println(class1);
+	   
+		 
+		
+		
+		
+		
 	}
 
 }
